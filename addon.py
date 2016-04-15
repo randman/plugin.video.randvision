6b0278bd338e3fc07da76f176b21d8139c6d7d85
randman <rand_anderson@yahoo.com>
2016-04-15 14:11:59 -0700

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,36 +1,6 @@
-import sys
-import urllib
-import urlparse
-import xbmcgui
-import xbmcplugin
+import xbmcaddon, util
 
-base_url = sys.argv[0]
-addon_handle = int(sys.argv[1])
-args = urlparse.parse_qs(sys.argv[2][1:])
+addon = xbmcaddon.Addon('plugin.video.randvision')
 
-xbmcplugin.setContent(addon_handle, 'movies')
-
-def build_url(query):
-    return base_url + '?' + urllib.urlencode(query)
-
-mode = args.get('mode', None)
-
-if mode is None:
-    url = build_url({'mode': 'folder', 'foldername': 'Folder One'})
-    li = xbmcgui.ListItem('Folder One', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
-
-    url = build_url({'mode': 'folder', 'foldername': 'Folder Two'})
-    li = xbmcgui.ListItem('Folder Two', iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
-                                listitem=li, isFolder=True)
-
-    xbmcplugin.endOfDirectory(addon_handle)
-
-elif mode[0] == 'folder':
-    foldername = args['foldername'][0]
-    url = 'http://localhost/some_video.mkv'
-    li = xbmcgui.ListItem(foldername + ' Video', iconImage='DefaultVideo.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
+util.playMedia(addon.getAddonInfo('name'), addon.getAddonInfo('icon'), 
+               'rtmp://stream2.france24.yacast.net:80/france24_live/fr playpath=f24_livefr app=france24_live/fr')
