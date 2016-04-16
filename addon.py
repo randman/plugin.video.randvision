6e6f67de89d5b508a36608af189b865ad7a41863
randman <rand_anderson@yahoo.com>
2016-04-16 12:42:10 -0700

    Update addon.py

--- addon.py
+++ addon.py
@@ -10,31 +10,40 @@ from xml.etree import ElementTree
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
 base_url = sys.argv[0]
+
+
+
+
 #addon_handle = int(sys.argv[1])
 myserver = addon.getSetting('NLSSServer');
 user = addon.getSetting('User');
 pword = addon.getSetting('Password');
 cust = addon.getSetting('Customer');
-newSTream="rtmp://livestream.cdn-surfline.com/cdn-live-s3"
-neFile="/surfline/secure/live/wc-pontohdcam?e=0&h=a984acb31092dcb45648e5514c47941d"
-chose=1
-breaks=[]
-breaks.append(["/surfline/secure/live/wc-delmarcam?e=0&h=c0db264dadf5f645aa57c8b181d2193d", "Del Mar"])
-breaks.append(["/surfline/secure/live/wc-cardiffcam?e=0&h=7bb47612d894e43259ce72979572940e","Cardiff Reef"])
-breaks.append(["/surfline/secure/live/wc-beaconscam?e=0&h=5c65c0b105a9ba8636ca37071c03204e", "Beacons"])
-breaks.append(["/surfline/secure/live/wc-grandviewcam?e=0&h=dbfcb78a983dab5bfb1764b302048ee1","Grandview"])
-breaks.append(["/surfline/secure/live/wc-pontohdcam?e=0&h=a984acb31092dcb45648e5514c47941d","Ponto"])
-breaks.append(["/surfline/secure/live/wc-tamarackcam?e=0&h=d8959b95c5b27ab9249b4a09ec294f63","Tamarack"])
-breaks.append(["/surfline/secure/live/wc-oceansidecacam?e=0&h=104586ca27a46edc0bf443950a326acb", "Oceanside Harbor"])
-breaks.append(["/surfline/secure/wc-oceansidepiernscam?e=0&h=981d3492043d2d6e878066cc9a0768f4", "Oceanside Pier North"])
-breaks.append(["/surfline/secure/live/wc-osidepiersscam?e=0&h=d0cd50a3a2f78fb7bba3f14c90b8ab28","Oceanside Pier South"])
-neFile=breaks[chose][0]
-mybreak=breaks[chose][1]
-xbmcgui.Dialog().ok(addonname, mybreak,newSTream,neFile)
-item = xbmcgui.ListItem("Test")
-item.setProperty("PlayPath", neFile)
-xbmc.Player().play(newSTream, item)
 
+
+
+if myserver == 'surfline':
+    newSTream="rtmp://livestream.cdn-surfline.com/cdn-live-s3"
+    neFile="/surfline/secure/live/wc-pontohdcam?e=0&h=a984acb31092dcb45648e5514c47941d"
+    chose=user
+    breaks=[]
+    breaks.append(["/surfline/secure/live/wc-delmarcam?e=0&h=c0db264dadf5f645aa57c8b181d2193d", "Del Mar"])
+    breaks.append(["/surfline/secure/live/wc-cardiffcam?e=0&h=7bb47612d894e43259ce72979572940e","Cardiff Reef"])
+    breaks.append(["/surfline/secure/live/wc-beaconscam?e=0&h=5c65c0b105a9ba8636ca37071c03204e", "Beacons"])
+    breaks.append(["/surfline/secure/live/wc-grandviewcam?e=0&h=dbfcb78a983dab5bfb1764b302048ee1","Grandview"])
+    breaks.append(["/surfline/secure/live/wc-pontohdcam?e=0&h=a984acb31092dcb45648e5514c47941d","Ponto"])
+    breaks.append(["/surfline/secure/live/wc-tamarackcam?e=0&h=d8959b95c5b27ab9249b4a09ec294f63","Tamarack"])
+    breaks.append(["/surfline/secure/live/wc-oceansidecacam?e=0&h=104586ca27a46edc0bf443950a326acb", "Oceanside Harbor"])
+    breaks.append(["/surfline/secure/wc-oceansidepiernscam?e=0&h=981d3492043d2d6e878066cc9a0768f4", "Oceanside Pier North"])
+    breaks.append(["/surfline/secure/live/wc-osidepiersscam?e=0&h=d0cd50a3a2f78fb7bba3f14c90b8ab28","Oceanside Pier South"])
+    neFile=breaks[chose][0]
+    mybreak=breaks[chose][1]
+    xbmcgui.Dialog().ok(addonname, mybreak,newSTream,neFile)
+    item = xbmcgui.ListItem("Test")
+    item.setProperty("PlayPath", neFile)
+    xbmc.Player().play(newSTream, item)
+
+#addon.add_video_item({'url': newSTream},{'title': 'Del Mar'},{'PlayPath': '/surfline/secure/live/wc-delmarcam?e=0&h=c0db264dadf5f645aa57c8b181d2193d'})
 #base_url = sys.argv[0]
 #addon_handle = int(sys.argv[1])
 #args = urlparse.parse_qs(sys.argv[2][1:])
